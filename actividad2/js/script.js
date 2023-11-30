e7a8b0650885ff3d4321a9edd1379fca20c3993c
Albertoprograming <134186137+Albertoprograming@users.noreply.github.com>
2023-11-30 00:48:23 -0600

    Add files via upload

--- actividad2/js/script.js
+++ actividad2/js/script.js
@@ -1,51 +1,155 @@
-// Obtén una referencia a los botones del juego
-const buttons = document.querySelectorAll('.cuadricula button');
-const playButton = document.querySelector('.footer button');
-const mensaje = document.querySelector('.mensaje');
+document.addEventListener('DOMContentLoaded', function() {
+  const botones = document.querySelectorAll('.cuadricula button');
+  const boton_jugar = document.querySelector('.footer button');
+  const contMovimientos = document.querySelector('.footer span:nth-child(2)');
+  const contime = document.querySelector('.footer span:nth-child(3)');
 
-// Posición actual del espacio vacío
-let emptyPosition = 15; // El último botón está vacío al principio
+  let miPuzzle; // Variable para guardar la instancia del juego
 
-// Mezcla las fichas en el juego
-const shuffle = () => {
-  for (let i = 0; i < 1000; i++) {
-    const randomIndex = Math.floor(Math.random() * 4);
-    const newIndex = emptyPosition + [-1, 1, -4, 4][randomIndex];
+  boton_jugar.addEventListener('click', function() {
+    // Crear una nueva instancia del juego cuando se presione el botón Jugar
+    const sizeTablero = 4; // Define el tamaño del tablero (cambia esto según tus necesidades)
+    const miTiempo = new tiempo(); // Instancia de tiempo
+    const miTablero = new tablero(sizeTablero); // Instancia de tablero
 
-    if (newIndex >= 0 && newIndex < 16) {
-      buttons[emptyPosition].textContent = buttons[newIndex].textContent;
-      buttons[newIndex].textContent = '';
-      emptyPosition = newIndex;
+    miPuzzle = new Puzzle(miTablero, miTiempo, contMovimientos);
+
+    // Inicializar el juego
+    miPuzzle.inicio();
+  });
+
+  class tiempo {
+    constructor() {
+      this.segundos = 0;
+      this.intervalo = null;
+    }
+
+    iniciartime() {
+      this.intervalo = setInterval(() => {
+        this.segundos++;
+        contime.textContent = `Tiempo: ${this.segundos}`;
+      }, 1000); // Actualizar cada segundo
     }
   }
-};
 
-// Función para verificar si se ha ganado el juego
-const checkWin = () => {
-  const order = Array.from(buttons).map((button) => button.textContent);
-  if (order.join('') === '123456789101112131415') {
-    mensaje.textContent = '¡Ganaste!';
+  class pieza {
+    constructor() {
+      // Propiedades de las piezas, si son necesarias
+    }
   }
-};
 
-// Manejar el clic en las fichas del juego
-buttons.forEach((button, index) => {
-  button.addEventListener('click', () => {
-    if (Math.abs(emptyPosition - index) === 1 || Math.abs(emptyPosition - index) === 4) {
-      buttons[emptyPosition].textContent = button.textContent;
-      button.textContent = '';
-      emptyPosition = index;
-      checkWin();
+  class tablero {
+    constructor(size) {
+      this.size = size;
+      this.piezas = []; // Almacenar las piezas del rompecabezas
+    }
+  
+    crearpiezas() {
+      for (let i = 0; i < this.size * this.size - 1; i++) {
+        const unaPieza = new pieza(); // Crear una instancia de pieza
+        this.piezas.push(unaPieza);
+      }
+      this.piezas.push(null); // La última pieza será la pieza vacía
+    }
+  
+    mezclar() {
+      for (let i = this.piezas.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [this.piezas[i], this.piezas[j]] = [this.piezas[j], this.piezas[i]];
+      }
+    }
+
+    moverPieza(direccion) {
+      // Encuentra la posición de la pieza vacía (null)
+      const posicionVacia = this.piezas.findIndex(pieza => pieza === null);
+
+      // Encuentra las coordenadas x e y de la pieza vacía
+      const filaVacia = Math.floor(posicionVacia / this.size);
+      const columnaVacia = posicionVacia % this.size;
+
+      let nuevaFila = filaVacia;
+      let nuevaColumna = columnaVacia;
+
+      // Actualiza las coordenadas según la dirección
+      switch (direccion) {
+        case 'arriba':
+          nuevaFila--;
+          break;
+        case 'abajo':
+          nuevaFila++;
+          break;
+        case 'izquierda':
+          nuevaColumna--;
+          break;
+        case 'derecha':
+          nuevaColumna++;
+          break;
+      }
+
+      // Verifica si la nueva posición está dentro del tablero
+      if (nuevaFila >= 0 && nuevaFila < this.size && nuevaColumna >= 0 && nuevaColumna < this.size) {
+        // Calcula la nueva posición en el array de piezas
+        const nuevaPosicion = nuevaFila * this.size + nuevaColumna;
+
+        // Intercambia la pieza vacía con la pieza adyacente en la dirección indicada
+        [this.piezas[posicionVacia], this.piezas[nuevaPosicion]] = [this.piezas[nuevaPosicion], this.piezas[posicionVacia]];
+
+        // El movimiento fue válido
+        return true;
+      }
+
+      // El movimiento no fue válido
+      return false;
+    }
+  }
+
+  class Puzzle {
+    constructor(tablero, tiempo, contmov) {
+      this.tablero = tablero;
+      this.tiempo = tiempo;
+      this.contmov = contmov;
+    }
+
+    inicio() {
+      this.tablero.crearpiezas();
+      this.tablero.mezclar();
+      this.tiempo.iniciartime();
+    }
+
+    actualizarInterfaz() {
+      botones.forEach((boton, indice) => {
+        if (this.tablero.piezas[indice] !== null) {
+          boton.textContent = this.tablero.piezas[indice].valor; // Asigna el valor de la pieza al botón
+        } else {
+          boton.textContent = ''; // La pieza vacía no tiene texto
+        }
+      });
+    }
+  }
+
+  document.addEventListener('keydown', function(event) {
+    if (miPuzzle && miPuzzle.tablero) {
+      let movimientoValido = false;
+
+      switch(event.key) {
+        case 'ArrowUp':
+          movimientoValido = miPuzzle.tablero.moverPieza('arriba');
+          break;
+        case 'ArrowDown':
+          movimientoValido = miPuzzle.tablero.moverPieza('abajo');
+          break;
+        case 'ArrowLeft':
+          movimientoValido = miPuzzle.tablero.moverPieza('izquierda');
+          break;
+        case 'ArrowRight':
+          movimientoValido = miPuzzle.tablero.moverPieza('derecha');
+          break;
+      }
+
+      if (movimientoValido) {
+        // Actualizar la interfaz si el movimiento es válido
+        miPuzzle.actualizarInterfaz();
+      }
     }
   });
 });
-
-// Manejar clic en el botón "Jugar" para mezclar las fichas
-playButton.addEventListener('click', () => {
-  mensaje.textContent = '';
-  emptyPosition = 15; // Restablecer la posición del espacio vacío
-  shuffle();
-});
-
-// Mezclar las fichas al cargar la página
-shuffle();
